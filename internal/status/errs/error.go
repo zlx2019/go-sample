7bc0261e4f1fab437cf14e4e00551fa956f4d6b1
Zero <15133024857@163.com>
2024-08-12 00:24:06 +0800

    feature: add errs wrap

--- internal/status/errs/error.go
+++ internal/status/errs/error.go
@@ -1,6 +1,9 @@
 package errs
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Error 服务业务错误
 type Error struct {
@@ -17,4 +20,13 @@ func (be *Error) Error() string {
 // 构建错误
 func of(code int, msg string) *Error {
 	return &Error{code, msg}
+}
+
+// Wrap 将业务错误 和标准错误包装，合并为错误链.
+func Wrap(e *Error, err error) error {
+	return errors.Join(e, err)
+}
+
+func Is(src error, dst error) bool {
+	return errors.Is(src, dst)
 }
